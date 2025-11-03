0af4fe927a63dee553739409f2d0d85899fa880c
Shawn D'silva <shawn.dsilva_97@outlook.com>
2025-11-03 12:59:50 +0000

    Override TokenObtainPairView to return HTTP Only Cookies

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,5 +1,10 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from django.conf import settings
+from rest_framework.views import APIView, Response
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 class ProtectedView(APIView):
@@ -7,3 +12,34 @@ class ProtectedView(APIView):
     def get(self, request):
         content = {"message": "This is a protected resource."}
         return Response(content)
+
+
+class LoginView(TokenObtainPairView):
+    def post(self, request, *args, **kwargs):
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            access_token = response.data.get("access")
+            refresh_token = response.data.get("refresh")
+
+            response.set_cookie(
+                "access",
+                access_token,
+                max_age=settings.AUTH_COOKIE_ACCESS_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                secure=settings.AUTH_COOKIE_SECURE,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE,
+            )
+
+            response.set_cookie(
+                "refresh",
+                refresh_token,
+                max_age=settings.AUTH_COOKIE_REFRESH_MAX_AGE,
+                path=settings.AUTH_COOKIE_PATH,
+                secure=settings.AUTH_COOKIE_SECURE,
+                httponly=settings.AUTH_COOKIE_HTTP_ONLY,
+                samesite=settings.AUTH_COOKIE_SAMESITE,
+            )
+
+        return response
