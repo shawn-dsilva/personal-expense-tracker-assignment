b2d08220d6ed49da0f6e7f5d27fd4c2ebb40274c
Shawn D'silva <shawn.dsilva_97@outlook.com>
2025-11-04 22:58:06 +0530

    Login card using shadcn added

---
+++ frontend/src/hooks/useLogin.ts
@@ -0,0 +1,36 @@
+import { useQueryClient, useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
+import { API_BASE_URL } from "../constant";
+
+interface User {
+  first_name?: string;
+  last_name?: string;
+  username?: string;
+}
+export function useLogin() {
+  const router = useRouter();
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data) => {
+      const res = await fetch(`${API_BASE_URL}/api/auth/login/`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      if (!res.ok) {
+        const data = await res.json();
+
+        console.log(data);
+        return data;
+      } else {
+        return null;
+      }
+    },
+    onSuccess: (user) => {
+      const { username, first_name, last_name } = user as User;
+      queryClient.setQueryData(["user"], { username, first_name, last_name });
+      router.push("/dashboard");
+    },
+  });
+}
--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
 
 const Login = () => {
     return (
-        <div>Login</div>
+        <Card className="m-auto w-sm">
+            <CardHeader className="text-center">
+                <CardTitle className="text-xl">Login</CardTitle>
+                <CardDescription>Enter your Credentials</CardDescription>
+            </CardHeader>
+            <CardContent>
+                <form method="post" onSubmit={handleSubmit}>
+                    <div className="grid gap-6">
+                        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t"></div>
+                        <div className="grid gap-6">
+
+                            <Button type="submit" className="w-full">
+                                {isLoading ? "loading..." : "Login"}
+                            </Button>
+                        </div>
+                    </div>
+                </form>
+            </CardContent>
+        </Card>
     )
 }
 
