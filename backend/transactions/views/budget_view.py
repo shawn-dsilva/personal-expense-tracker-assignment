f8f26a4a0a1f4b874cc8ded85f9e7c346e6c4721
Shawn D'silva <shawn.dsilva_97@outlook.com>
2025-11-10 21:06:53 +0530

    added view to compare budget to expenses

--- backend/transactions/views/budget_view.py
+++ backend/transactions/views/budget_view.py
@@ -1,5 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from transactions.models import Budget, Transaction
 from transactions.serializers import BudgetSerializer
 
 
@@ -10,3 +11,28 @@ class CreateBudgetView(APIView):
             serializer.save()
             return Response(serializer.data, status=201)
         return Response(serializer.errors, status=400)
+
+
+class BudgetStatisticsView(APIView):
+    def get(self, request, month, *args, **kwargs):
+        user = request.user.id
+        budget = Budget.objects.filter(user=user, date__month=month)
+        transactions = Transaction.objects.filter(
+            user=user, date__month=month
+        ).order_by("-date")
+
+        total_expense = sum(t.amount for t in transactions if t.type == "expense")
+
+        budget_difference = budget.amount - total_expense
+
+        summary = {
+            "total_expense": total_expense,
+            "budget": budget.amount,
+            "budget_difference": budget_difference,
+            "over_budget": False,
+        }
+
+        if total_expense > budget.amount:
+            summary["over_budget"] = True
+            return Response(summary, status=200)
+        return Response(summary, status=200)
